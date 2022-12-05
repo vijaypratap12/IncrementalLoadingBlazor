abb16922fc8f7cfbf9f4d1b0894e66badeeb3d3a
vijaypratap12 <vijaypratap12121999@gmail.com>
2022-12-05 18:59:05 +0530

    Initial Commit

---
+++ IncrementalLoadingDemo/Data/WeatherForecastService.cs
@@ -0,0 +1,20 @@
+namespace IncrementalLoadingDemo.Data
+{
+    public class WeatherForecastService
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate, int take =10)
+        {
+            return Task.FromResult(Enumerable.Range(1, take).Select(index => new WeatherForecast
+            {
+                Date = startDate.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            }).ToArray());
+        }
+    }
+}
